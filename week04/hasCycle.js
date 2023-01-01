050e60fff5eca3dc6c87d6c1d116f6431f0ec201
rafat17 <43632236+rafat17@users.noreply.github.com>
2023-01-01 14:46:51 +0600

    hasCycle solution

---
+++ week04/hasCycle.js
@@ -0,0 +1,31 @@
+// Time Complexity: O(n), n = number of nodes in the list
+// Space Complexity: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    let slowPointer = head
+    let fastPointer = head
+
+    while (slowPointer !== null && fastPointer !== null && fastPointer.next !== null) {
+
+        slowPointer = slowPointer.next
+        fastPointer = fastPointer.next.next
+
+        if (slowPointer === fastPointer) {
+            return true
+        }
+    }
+
+    return false
+};
